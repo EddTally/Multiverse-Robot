02f19b486fa73d8f2af975e232baf88e59f171c6
Edward-Heales <edward.tallentire@heales.com>
2023-01-14 20:40:33 +0000

    split up the getting state and grid functions so they could be tested individually and ran some tests

--- lib/robot.ex
+++ lib/robot.ex
@@ -5,41 +5,78 @@ defmodule MultiverseRobot do
 
 	@doc """
 		Process all input commands to determine the position of the robot in a grid of user defined x y limits and movements.
+		It calls the set_robot_grid/0 and set_robot_commands/0 functions to take user input.
+
+		## Parameters
+		To be able to test this function we need to provide a dummy state that isn't from the command line.
+		- test? - Boolean, whether this function being used as a test or not
+		- state - Map, the initial state of the robot
 	"""
-	def start_robot() do
+	@spec start_robot(boolean(), map()) :: map()
+	def start_robot(test? \\ false, state \\ %{}) do
+		state = case test? do
+			true
+				-> state
+			false ->
+				{grid_x, grid_y} = set_robot_grid()
+				set_robot_state(grid_x, grid_y)
+
+			end
+		process_moves(state.movements, state)
+
+	end
+
+	@doc """
+	set_robot_grid will ask the user to input two numbers that define the limits of the grid
+	"""
+	@spec set_robot_grid() :: tuple()
+	def set_robot_grid() do
 		input =	IO.gets("Please enter the limits of the robots grid in the form 'X Y'\n")
 		<<grid_x::binary-size(1), _::binary-size(1), grid_y::binary-size(1), _::bitstring>> = input
-		IO.puts("You entered #{grid_x}, #{grid_y}")
-		# Initial robot state
-		#(2, 3, E) LFRFF
-		input = IO.gets("Please enter the initial state of the robots in the form '(X, Y, Direction) Movements'\n")
-		 |> String.replace("(", "")
-		 |> String.replace(")", "")
-		 |> String.replace(",", "")
-		 |> String.replace(" ", "")
-		 |> String.trim()
-		 IO.inspect(input)
-		<<pos_x::binary-size(1),pos_y::binary-size(1), direction::binary-size(1), movements::bitstring>> = input
-		state = %{
-			pos_x: String.to_integer(pos_x),
-			pos_y: String.to_integer(pos_y),
-			grid_max_x: String.to_integer(grid_x),
-			grid_max_y: String.to_integer(grid_y),
+		#IO.puts("You entered #{grid_x}, #{grid_y}")
+		{grid_x, grid_y}
+	end
+
+	@doc """
+	set_robot_state will ask the user to input two numbers that define the limits of the grid
+	"""
+	@spec set_robot_state(integer(), integer()) :: String.t()
+	def set_robot_state(grid_x, grid_y) do
+ 		input = IO.gets("Please enter the initial state of the robots in the form '(X, Y, Direction) Movements'\n")
+			|> String.replace("(", "")
+			|> String.replace(")", "")
+			|> String.replace(",", "")
+			|> String.replace(" ", "")
+			|> String.trim()
+
+		<<pos_x::binary-size(1),pos_y::binary-size(1), direction::binary-size(1), movements::bitstring>>= input
+			%{
+			pos_x: pos_x,
+			pos_y: pos_y,
+			grid_max_x: grid_x,
+			grid_max_y: grid_y,
 			direction: direction,
 			movements: movements,
 			lost?: false
 		}
-		process_moves(movements, state)
-
 	end
 
+	@doc """
+		process_moves takes in a bitstring with all of the moves inputted by the user
+	"""
+	@spec process_moves(bitstring(), map()) :: map()
 	def process_moves(<<0::0>>, state) do
-		state = if state.pos_x > state.grid_max_x, do: %{state | lost?: true}, else: state
-		state = if state.pos_y > state.grid_max_y, do: %{state | lost?: true}, else: state
-		state
+		result = cond do
+			state.pos_y > state.grid_max_y -> true
+			state.pos_x > state.grid_max_x -> true
+			state.pos_x < 0 -> true
+			state.pos_y < 0 -> true
+			true -> false
+		end
+		%{state | lost?: result}
 	end
+	@spec process_moves(bitstring(), map()) :: map()
 	def process_moves(<<move::binary-size(1), rest::bitstring>>, state) do
-		IO.inspect(state)
 		state = case move do
 			"L" -> %{state | pos_x: state.pos_x - 1}
 			"R" -> %{state | pos_x: state.pos_x + 1}
--- test/multiverse_robot_test.exs
+++ test/multiverse_robot_test.exs
@@ -1,15 +1,50 @@
 defmodule MultiverseRobotTest do
+  alias ExUnit.CaptureIO
   use ExUnit.Case
   doctest MultiverseRobot
 
 	import ExUnit.CaptureIO
 
+	@full_example_input "2, 1\n(2, 1, N) LRFFFFFFFFFRBL"
 
   test "robot_example" do
-    input = "2, 1\n(2, 1, N) LRFFFFFFFFFRBL"
+		state = %{
+			pos_x: 2,
+			pos_y: 1,
+			grid_max_x: 5,
+			grid_max_y: 6,
+			direction: N,
+			movements: "LRFFFFFFFFFRBL",
+			lost?: false
+		}
 
-		assert capture_io([input: input, capture_prompt: false], fn ->
-			IO.write MultiverseRobot.start_robot()
-		end) == "You entered 2, 1\n21NLRFFRBL"
+		assert MultiverseRobot.start_robot(true, state) ==
+			%{
+  			direction: N,
+  			grid_max_x: 5,
+  			grid_max_y: 6,
+  			lost?: true,
+  			movements: "LRFFFFFFFFFRBL",
+  			pos_x: 2,
+  			pos_y: 9
+			}
   end
+
+	test "robot grid setup" do
+		assert CaptureIO.capture_io("2 1", fn ->
+			MultiverseRobot.set_robot_grid()
+		end) =="Please enter the limits of the robots grid in the form 'X Y'\n"
+
+		# We don't care about the prompt on this one only the output
+		# assert CaptureIO.capture_io([input: "2 1", capture_prompt: false], fn ->
+		# 	MultiverseRobot.set_robot_grid()
+		# end) =={2, 1}
+	end
+
+	test "robot state setup" do
+		assert CaptureIO.capture_io("(2, 1, N) LRFFFFFFFFFRBL", fn ->
+			MultiverseRobot.set_robot_state(2, 1)
+		end) =="Please enter the initial state of the robots in the form '(X, Y, Direction) Movements'\n"
+
+	end
 end
